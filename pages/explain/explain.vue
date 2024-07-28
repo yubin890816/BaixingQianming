691ad61b8605dec5d764fec3259651bb4118c6eb
yubin <yubin890816@163.com>
2024-07-28 12:11:13 +0800

    init

---
+++ pages/explain/explain.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		解释姓名
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/generate/generate.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		生成姓名
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
